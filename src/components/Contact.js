38c4cda4b76d23809557e7c92ddbb6fe7af4a349
Sagar Kathuria <sagar.kathuria@4mation.local>
2019-07-16 16:47:27 +1000

    Chapter - 23 - State Change

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,27 +3,28 @@ import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "../../node_modules/@fortawesome/react-fontawesome";
 
 class Contact extends Component {
-  state = {};
-
-  onShowClick = (name, e) => {
-    // console.log("OnShowClick Function");
-    console.log(name);
+  state = {
+    showContactInfo: true
+  };
+  onShowClick = e => {
+    this.setState({
+      showContactInfo: !this.state.showContactInfo
+    });
   };
   render() {
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
     return (
       <div className="card card-body mb-3">
         <h4>
-          {name}{" "}
-          <FontAwesomeIcon
-            onClick={() => this.onShowClick(name)}
-            icon="sort-down"
-          />
+          {name} <FontAwesomeIcon onClick={this.onShowClick} icon="sort-down" />
         </h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
